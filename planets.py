139d1e9c80a14d58472261e07b3c34c598f8984f
Ryan Burns <rdburns@gmail.com>
2014-05-14 20:16:11 -0500

    Refactored

--- planets.py
+++ planets.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
-import xml.etree.ElementTree as ET, urllib, gzip, io
-url = "https://github.com/OpenExoplanetCatalogue/oec_gzip/raw/master/systems.xml.gz"
-oec = ET.parse(gzip.GzipFile(fileobj=io.BytesIO(urllib.urlopen(url).read())))
+import xml.etree.ElementTree as ET
+import urllib
+import gzip
+import io
 
-# Output mass and radius of all planets 
-for planet in oec.findall(".//planet"):
-    print [planet.findtext("mass"), planet.findtext("radius")]
+def demo():
+    # Output mass and radius of all planets 
+    for planet in oec.findall(".//planet"):
+        print [planet.findtext("mass"), planet.findtext("radius")]
 
-# Find all circumbinary planets 
-for planet in oec.findall(".//binary/planet"):
-    print planet.findtext("name")
+    # Find all circumbinary planets 
+    for planet in oec.findall(".//binary/planet"):
+        print planet.findtext("name")
 
-# Output distance to planetary system (in pc, if known) and number of planets in system
-for system in oec.findall(".//system"):
-    print system.findtext("distance"), len(system.findall(".//planet"))
+    # Output distance to planetary system (in pc, if known) and number of planets in system
+    for system in oec.findall(".//system"):
+        print system.findtext("distance"), len(system.findall(".//planet"))
+
+
+def get_etree():
+    url = "https://github.com/OpenExoplanetCatalogue/oec_gzip/raw/master/systems.xml.gz"
+    fo = io.BytesIO(urllib.urlopen(url).read()
+    tree = ET.parse(gzip.GzipFile(fileobj=fo)))
+    return tree
+
