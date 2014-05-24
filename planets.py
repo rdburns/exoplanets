b5a35107a9ce16683b3e341666506f0b921402b4
Ryan Burns <rdburns@gmail.com>
2014-05-23 20:45:02 -0400

    Added Star letter label to ascii art.

--- planets.py
+++ planets.py
@@ -346,6 +346,7 @@ def ascii_system(system):
         t = [' ']*80
         d = [' ']*80
         d[0] = spectral_colorize('*', star)
+        t[0] = star.find('name').text[-1]
         for planet in star.iterfind('planet'):
             sma = float(planet.find('semimajoraxis').text)
             loc = int((sma / maxsma) * 78) + 1
